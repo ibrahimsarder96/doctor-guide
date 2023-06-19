515e69ca68c127380ac70a7c025972faf1046e94
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-06-19 23:59:16 +0600

    doctor shedule add

--- src/Pages/Home/Home/DoctorShedule.js
+++ src/Pages/Home/Home/DoctorShedule.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import doctor2  from '../../../assets/doctor2.png';
-const OnlineBest = () => {
+import doctor3  from '../../../assets/doctor3.png';
+const DoctorShedule = () => {
   return (
     <div class="grid grid-cols-2 gap-12 relative">
-  <div>
-    <div className="indicator-item indicator-top absolute  indicator-start badge pt-16 text-accent border-none badge-white">
-    <h1>top+startdaigjhoijgfajkldsh</h1>
-    <h1>top+startdaigjhoijgfajkldsh</h1>
-    </div>
-  <img className='h-4/6' src={doctor2} alt="" srcset="" />
-  </div>
   <div className='mt-72'>
     <h1 className='text-3xl font-medium text-accent'>Sick Kid? How to Deal with a</h1>
     <h2 className='text-3xl font-medium text-accent'>Child's Fever: 4 Steps</h2>
@@ -18,8 +11,14 @@ const OnlineBest = () => {
     <p className='text-accent my-4'>Nibh turpis praesent pretium</p>
     <p className='text-accent my-4'>Cras sit sed nec a nunc</p>
   </div>
+  <div>
+    <div className="indicator-item indicator-top absolute  indicator-start badge pt-16 text-accent border-none badge-white">
+    <h1>top+start</h1>
+    </div>
+  <img className='h-4/6' src={doctor3} alt="" srcset="" />
+  </div>
 </div>
   );
 };
 
-export default OnlineBest;
+export default DoctorShedule;
--- src/Pages/Home/Home/Home/Home.js
+++ src/Pages/Home/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner';
 import OnlineBest from '../OnlineBest';
+import DoctorShedule from '../DoctorShedule';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <OnlineBest></OnlineBest>
+      <DoctorShedule></DoctorShedule>
     </div>
   );
 };
